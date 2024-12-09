048a5a1dc3cb42d6906863882bb9d54952cb4cb2
jmechavez <jmechaveznet@zoho.com>
2024-12-09 10:12:00 +0800

    added postgres using docker-compose

--- insurance/domain/clients.go
+++ insurance/domain/clients.go
@@ -1,17 +1,17 @@
 package domain
 
 type Client struct {
-	Fname        string `json:"fname"`
-	Lname        string `json:"lname"`
-	Birthdate    string `json:"birthdate"`
-	IdCard1      string `json:"id_card_1"`
-	IdNo1        string `json:"id_no_1"`
-	IdCard2      string `json:"id_card_2"`
-	IdNo2        string `json:"id_no_2"`
-	BirthOfPlace string `json:"birth_of_place"`
-	ContactNo    string `json:"contact_no"`
-	Status       int    `json:"status"`
-	Gender       string `json:"gender"`
+	Fname      string `json:"fname"`
+	Lname      string `json:"lname"`
+	Birthdate  string `json:"birthdate"`
+	IdCard1    string `json:"id_card_1"`
+	IdNo1      string `json:"id_no_1"`
+	IdCard2    string `json:"id_card_2"`
+	IdNo2      string `json:"id_no_2"`
+	BirthPlace string `json:"birthplace"`
+	ContactNo  string `json:"contact_no"`
+	Status     int    `json:"status"`
+	Gender     string `json:"gender"`
 }
 
 type ClientRepository interface {
---
+++ insurance/resources/database.sql
@@ -0,0 +1,26 @@
+-- Create the database
+CREATE DATABASE insurance;
+\c insurance
+
+-- Drop and create `customers` table
+DROP TABLE IF EXISTS clients;
+CREATE TABLE clients (
+  client_id SERIAL PRIMARY KEY,
+  fname VARCHAR(100) NOT NULL,
+  lname VARCHAR(100) NOT NULL,
+  birthdate DATE NOT NULL, 
+  id_card_1 VARCHAR(100) NOT NULL, 
+  id_no_1 VARCHAR(100) NOT NULL,
+  id_card_2 VARCHAR(100) NOT NULL, 
+  id_no_2 VARCHAR(100) NOT NULL,
+  birthplace VARCHAR(100) NOT NULL,
+  contact_no VARCHAR(30) NOT NULL,
+  status BOOLEAN NOT NULL DEFAULT TRUE,
+  gender VARCHAR(20) NOT NULL
+);
+
+-- Insert data into `customers` table
+INSERT INTO clients (client_id, fname, lname, birthdate, id_card_1, id_no_1, id_card_2, id_no_2, birthplace, contact_no, status, gender) VALUES
+  (110001, 'John Michael', 'Echavez', '1990-09-16', 'National Id', '001-001-001', 'Driver License', '001-002-002', 'General Santos City', '09000000001', TRUE, 'Male'),
+  (110002, 'JM', 'Echavez', '1990-09-16', 'National Id', '002-002-002', 'Driver License', '002-002-002', 'Paranaque', '09000000002', TRUE, 'Male');
+
